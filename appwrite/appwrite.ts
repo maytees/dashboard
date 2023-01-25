a2f6bf9fda8aca9ef50a6b3973f535a814b30837
Matees <mateespublicprofile@gmail.com>
2023-01-25 17:28:02 -0500

    appwrite.ts

---
+++ appwrite/appwrite.ts
@@ -0,0 +1,10 @@
+import * as appwrite from "appwrite";
+
+const CLIENT: appwrite.Client = new appwrite.Client();
+
+const PROJECTID: any = Deno.env.get("APPWRITE_PROJID")?.toString();
+const ENDPOINT: any = Deno.env.get("APPWRITE_ENDPOINT")?.toString();
+
+CLIENT.setEndpoint(ENDPOINT).setProject(PROJECTID);
+
+export const ACCOUNT: appwrite.Account = new appwrite.Account(CLIENT);
